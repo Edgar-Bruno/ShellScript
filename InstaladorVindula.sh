3b4319c94dcdb6b0f92e83191fcb860b4544ae28
Edgar Bruno <edgarbruno@liberiun.com>
2014-01-10 03:52:45 -0200

    Correções de Layout

--- InstaladorVindula.sh
+++ InstaladorVindula.sh
@@ -1,59 +1,50 @@
 #!/bin/bash
+
 cursorVI(){ sleep 0.25; echo -n "   -"; sleep 0.25; echo -n "> "; }
+
 menuPrincipal(){
 
 estiPrinci
 
-
- if [ -f /opt/vindula2.0/vindula/bin/instan-ce ]; then
-   
-    txtT=" Vindula, a sua INTRANET"
-    txtLb=" [1] - Iniciar a Intranet"
-    varVl=3
+ if [ -f /opt/vindula2.0/vindula/bin/instance ]; then
+  
+    txtT="Vindula, a sua INTRANET"
+    txtLb=" [1] - Iniciar a Intranet        "
+    varVl=300
 
             estiApro
             baseLayout
-      
  else
 
-    txtT=" Instalador o Vindula  "
-    txtLb=" [1] - Instalar a Intranet" 
-    varVl=2
+    txtT="Instalador o Vindula   "
+    txtLb=" [1] - Instalar a Intranet       " 
+    varVl=200
             estiInst
-            txtSc=";0"
             baseLayout  
-         
- fi
+  fi
 
 cursorVI
 read opcD;
 echo -e "\a"
 
         if [ "$opcD" == 0 ]; then
-            opcE=0
-
+            opcE=100
         else
             opcE=$(($varVl-$opcD))
-
         fi  
 
 case "$opcE" in
-    1)
-             
-        
+
+    199)
         confirmarInt
         ;;
-    2)
-        
+    299)
         aguardIni
         ;;
-    0)
-        
+    100)
         estiSair
         ;;
     *)
- 
-        
         opcInvalida
         menuPrincipal
         ;;
@@ -62,7 +53,6 @@ esac
 }
 
 instalarVindula(){
-
 
 clear;
 
@@ -102,8 +92,6 @@ cd /opt/vindula2.0/vindula/
 
 ./bin/buildout  -vN
 
-chmod 777 -R /opt/vindula2.0/
-
 menuPrincipal
 
 }
@@ -114,30 +102,32 @@ cd /
  ./opt/vindula2.0/vindula/bin/instance start
  sleep 5;
  x-www-browser localhost:8080/vindula/&
-
-
 }
 
 baseLayout(){
 
-echo -e " \n \e[${coRa}m \e[m \e[${coRaB}${txtSt}m ${txtT} \e[m \e[${coRaB}m  \e[m \e[${coRaB}m \e[m \e[${coRaB}m \e[m"    
-echo -e " \e[${coRa}m \e[m \e[${txtSb}m ${txtLd} \e[m" 
-echo -e " \e[${coRa}m \e[m \e[${txtSa}m ${txtDi} \e[m"
-echo -e " \e[${coRa}m \e[m \e[${txtSb}m ${txtLa} \e[m"
-echo -e " \e[${coRa}m \e[m \e[${txtSb}m ${txtLb} \e[m"
-echo -e " \e[${coRa}m \e[m \e[${txtSc}m ${txtLc} \e[m \n"
+echo -e "\n  \e[40m                                    \e[m"
+echo -e "  \e[40m \e[m\e[${coRa}m \e[m\e[40m \e[m\e[${coRaB}${txtSt}m ${txtT} \e[m\e[40m \e[m\e[${coRaB}m  \e[m\e[40m \e[m\e[${coRaB}m \e[m\e[40m \e[m\e[${coRaB}m \e[m\e[40m \e[m"    
+echo -e "  \e[40m \e[m\e[${coRa}m \e[m\e[${txtSd}m${txtLd} \e[m" 
+echo -e "  \e[40m \e[m\e[${coRa}m \e[m\e[${txtSa}m${txtDi} \e[m"
+echo -e "  \e[40m \e[m\e[${coRa}m \e[m\e[${txtSb}m${txtLa} \e[m"
+echo -e "  \e[40m \e[m\e[${coRa}m \e[m\e[${txtSb}m${txtLb} \e[m"
+echo -e "  \e[40m \e[m\e[${coRa}m \e[m\e[${txtSc}m${txtLc} \e[m"
+echo -e "  \e[40m                                    \e[m \n"
 
 }
 
 opcInvalida(){
 
         clear
+
+        txtLd="                                 "
+        txtLa=" --------------------------------"
         txtT=" !!! OPÇÃO INVÁLIDA !!!"
-        txtDi="    Essa opção não exite!      "
-        txtLb="Escolha uma das opções validas "
-        txtLc="no menu principal."
-        txtLa=""
-        txtLd=""
+        txtDi="      Essa opção não exite!      "
+        txtLb=" Escolha uma das opções validas  "
+        txtLc=" no menu principal.              "
+       
 
             estiExep
             baseLayout  
@@ -147,17 +137,7 @@ opcInvalida(){
         menuPrincipal
 }
 
-estiAlerta(){
-
-    coRa=43
-    coRaB=44
-    txtSa="40;33;1"
-}
-
 estiApro(){
-
-    txtSa="40;33;1"
-    txtSb="0"
 
     coRa=42
     coRaB=44
@@ -165,73 +145,70 @@ estiApro(){
 
 estiInst(){
 
-    txtSa="40;33;1"
-    txtSb="0"
-
-    txtSc=";1"
-
     coRa=41
-    coRaB=44
-
+    coRaB=46
 }
 
 estiExep(){
 
     coRa=43
     coRaB=41
-    txtSa="40;33;1"
-    txtSc=";0"
-}
+    txtSc="40;37;6"
 
+}
 
 estiSair(){
 
         clear
-        txtT=" A Liberiun agradece.  "
-        txtDi="    Essa opção não exite!      "
-        txtLd="Escolha uma das opções validas "
-        txtLa="Escolha uma das opções validas "
-        txtLb="no menu principal."
-        txtLc=""
-            
-            baseLayout 
 
-        sleep 2;
+        txtT=" A Liberiun agradece.  "
+        txtLd="  Obrigado por utilizar o Vindula"
+        txtDi=" Você gostaria de saber mais     "
+        txtLa=" sobre nossos serviços?          "
+        txtLb="                                 "
+        txtLc="   http://www.vindula.com.br     "
+
+        txtSd="40;37;1"
+        txtSa="40;37;6"
+
+            estiApro
+            baseLayout 
+            sleep 2;
+            exit;
 }
+
 estiPrinci(){
 
     clear
 
-    opcD=0
-    varVl=0
-
-    txtSa="40;37;1"
     txtSt=";37;1"
-    txtSb=";0"
-    txtSc=";0"
 
-    txtLd=""
-    txtDi=""
-    txtLa="-------------------------------"
-    txtLc=" [0] - Sair"
-    txtDi=" * Escolha uma opção no menu * "
-    
+    txtSd="40;31;1"
+    txtSa="40;33;1"
+    txtSb="40;37;6"
+    txtSc="40;37;6"
+
+    txtLd="                                 "
+    txtDi="  * Escolha uma opção no menu *  "
+    txtLa=" --------------------------------"
+    txtLc=" [0] - Sair                      "
+
 }
 
 confirmarInt(){
    
      clear
-        txtT=" Confirmar Instalação !"
-        txtLd="     -*- ATENÇÃO -*-           "
-        txtDi="Antes de instalar o Vindula,   "
-        txtLa="deve-se instalar as dependêcnias"
-        txtLb="necessárias"
-        txtLc="[s] - Sim | [n] - Não | [0] - Sair"
 
+        txtT=" Confirmar Instalação !"
+        txtLd="        -*- ATENÇÃO -*-          "
+        txtDi="   Antes de instalar o Vindula,  "
+        txtLa=" deve-se instalar as dependêcnias"
+        txtLb=" necessárias.                    "
+        txtLc=" [s]- Sim | [n]- Não | [0]- Sair "
+
+            txtSc="40;37;1"
             estiInst
             baseLayout  
-
-        sleep 2;
 
         cursorVI
         read opcI
@@ -239,21 +216,20 @@ confirmarInt(){
 
     case "$opcI" in
 
-    s)
-        aguardIni
-        menuPrincipal
-        ;;
-    n)        
-        menuPrincipal
-        ;;
-    0)
-        estiSair
-        ;;
-    *)
-        opcInvalida
-        menuPrincipal
-        ;;
-esac
+        s)
+            aguardIni
+            estiSair
+            ;;
+        n)        
+            menuPrincipal
+            ;;
+        0)
+            estiSair
+            ;;
+        *)
+            opcInvalida
+            ;;
+    esac
 
 }
 
@@ -262,37 +238,29 @@ aguardIni(){
      clear
 
      if [ "$opcI" != s ]; then
+
         txtT="Inicializando o Vindula"
-        txtLd="                               "
-        txtDi="   * Aguarde o carregamento *   "
-        txtLa="Dentro de instantes o navegador "
-        txtLb="de internet será iniciado."
-        txtLc=""
+        txtLa=" Dentro de instantes o navegador "
+        txtLb=" de internet será iniciado.      "
 
-           estiApro
-           baseLayout
-           
            executorInstancia 
-
      else
 
-        txtT="Inicializando a Instalação"
-        txtLd="                               "
-        txtDi="   * Aguarde o carregamento *   "
-        txtLa="Dentro de instantes a instalação"
-        txtLb="será iniciada."
-        txtLc=""
-        
-           estiApro
-           baseLayout
-           sleep 19;
+        txtT="Inicializando o Vindula"
+        txtT="Instalação do Vindula  "
+        txtLa=" Dentro de instantes a instalação"
+        txtLb=" será iniciada.                  "
+  
            instalarVindula         
+     fi 
+            txtLd="                                 "
+            txtDi="   * Aguarde o carregamento *    " 
+            txtLc="                                 "
 
-      fi 
-       
-       
-
+      
+       estiApro
+       baseLayout
+       sleep 2;
 }
-
 
 menuPrincipal    
