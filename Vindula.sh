b7c4db7c6b22ff61255ae34d2632747214f06f11
Edgar Bruno <edgarbruno@liberiun.com>
2014-02-25 16:44:51 -0300

    Script Inicial

---
+++ Vindula.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#IntranetVindula
+#
+#  Nesse Shell Script, é executado o processo de instalação e/ou execução 
+# da Intranet Vindula. 
+# Versão 1.00 - 25/02/2014
+#             - 
+#---------------------------------------------------------------------------
+
+
+wget -c "http://downloads.sourceforge.net/project/vindula/2.0.3/InstaladorVindula.sh"
+
+chmod +x InstaladorVindula.sh
+
+./InstaladorVindula.sh
+
+
+
