39abba30afbb6c7f30ab0150b7fea3a690824bc7
Edgar Bruno <edgar.brunos@gmail.com>
2017-02-14 21:34:42 +0000

    Otimização da leitura do arquivo de controle.

--- Diversos/FatiadorPastarG.sh
+++ Diversos/FatiadorPastarG.sh
@@ -18,6 +18,7 @@ pastaDestinoBase=$(echo $path"_"Fatiado)
 	# Diretório onde será criado subpastas e,  copiado os arquivos da pasta de original
 
 checkArquivosCP=$(echo $pastaDestinoBase"/"$pastaOrigin"_CP.txt")
+	# Arquivo que conterá todos os arquivos da pasta | Esse, será subdividido e esvaziado
 
 if [[ ! -d "$pastaDestinoBase" ]]; then
 	# Verifica se a subpasta não exite
@@ -39,11 +40,18 @@ if [[ ! -f $checkArquivosCP ]]; then
 			# O nome do arquivo é escrito com marcadores iniciais para posterior alteração após a cópia do mesmo
 		fi
 	done
+else
+	# Verifica se o processamento do arquivo foi concluído
+	echo -e "\nVerificando o arquivo de controle... \n"
+	
+	if [[ -f $(cat $checkArquivosCP | sed '/ -_- Processamento concluído -_- /!d') ]]; then
+		echo -e " *** Todos os arquivos foram movidos ***\n"
+		exit 0
+	fi
+
+
 fi
 
-
-arquivoCPLeitura=$(cat $checkArquivosCP | sed -n '/>>> /p')
- # Aqui é feita a leitura única do arquivo de controle capturando apenas os nomes de arquivos não movidos
 
 dNumero=0
  # Numero final do diretório fatiado
@@ -51,19 +59,83 @@ dNumero=0
 nArquivosMAX=999
  # Limitator de arquivos copiados por pasta
 
-arquivoCP="Null"
- # String com o nome do arquivo
 
-l=1
- # Linha do arquivo de controle aonde o arquivo está
+arquivoControleContador=0
+
+function leituraARQ()
+{
+	# Funcao para verificar e criar subdivisões do arquivo de controle
+	
+	checkarquivoControleContador=$(echo $pastaDestinoBase"/"$pastaOrigin"_CP_"$arquivoControleContador".txt")
+	# Cria caminho completo da subdivisão do primeiro arquivo
+
+	if [[ ! -f $checkarquivoControleContador ]]; then
+		
+		arquivoCPLeitura=$(cat $checkArquivosCP | sed '1, 999!d')
+		 # Recebe do arquivo de controle N linhas a fim de criar o subarquivo de controle
+
+		checkarquivoControleContador=$(echo $pastaDestinoBase"/"$pastaOrigin"_CP_"$arquivoControleContador".txt")
+			
+		echo "$arquivoCPLeitura" > $checkarquivoControleContador
+			recebeSaida=$$
+
+			echo "A Saida ----->>> $recebeSaida"
+			echo "______"$recebeSaida"_______"
+			# read -p "Press any key to continue... " -n1 -s
+			# sed '/^$/d' remove espaços em branco
+			# Cria o novo arquivo delimitado
+
+		echo -e "\nCriando aquivo de controle\n$checkarquivoControleContador\n"
+
+		echo -e "Removendo os nomes dos arquivos da listagem principal\n"
+		sed -i '1, 999d' $checkArquivosCP
+		
+		leituraARQ
+
+	else
+
+		echo -e " ---- Arquivo de sub leitura existente \n"
+		echo $checkarquivoControleContador
+
+		arquivoCPLeitura=$(cat $checkarquivoControleContador | sed -n '/>>> /p')
+
+		if [[ -z $arquivoCPLeitura ]]; then
+
+			echo " * Leitura sem resultados"
+			if [[ ! -z $recebeSaida ]]; then
+				echo " * Fim do processamento "
+				exit 0
+			else
+				recebeSaida=''
+				(( arquivoControleContador++ ))
+				leituraARQ
+			fi	
+		fi
+		
+	fi
+
+	# Verifica se o arquivo de controle existe
+	# arquivoCPLeitura=$(cat $checkArquivosCP | sed -n '/>>> /p')
+ 	# Aqui é feita a leitura única do arquivo de controle capturando apenas os nomes de arquivos não movidos
+}
+
 
 function main()
 {
 	arquivoCont=0
+	recebeSaida=''
 
-	while [[ ! -z "$arquivoCP" ]]
+	if [[ -z $arquivoCP ]]; then
+		leituraARQ
+		arquivoCP="Null"
+		# String com o nome do arquivo
+		l=1
+		# Linha do arquivo de controle aonde o arquivo está
+	fi
+
+	while [[ ! -z $arquivoCP ]]
+
 		do
-
 			arquivoCP=$(echo -e $arquivoCPLeitura \
 						| sed -e ''$l'!d' \
 						| sed 's/>>> //' \
@@ -76,34 +148,41 @@ function main()
 			if [[ ! -d "$pastaDestinoBase/Fatiado_$dNumero" ]]; then
 				# Verifica se a subpasta não exite
 				mkdir -pv "$pastaDestinoBase/Fatiado_$dNumero"
-				#echo -e "\n Diretório criado :\n $pastaDestinoBase"
+				
 			else
+
 				if [[ $arquivoCont -eq 0 ]]; then
 					arquivoCont=$(ls -l "$pastaDestinoBase/Fatiado_$dNumero" | grep ^- | wc -l)
+					# Comando que retorna a quantidade de arquivos de uma pasta
 				fi
 			fi
 
 			if [[ $arquivoCont -le $nArquivosMAX ]]; then
-				# Comando que retorna a quantidade de arquivos de uma pasta
 				if [[ ! -z "$arquivoCP" ]] ; then
-
+					
 					eval $(echo "rsync -avhr --progress $(echo -e "\"$path/$arquivoCP\"") $(echo -e "\"$pastaDestinoBase/Fatiado_$dNumero/$arquivoCP\"")")
-
-					sed -i "s/>>> ${arquivoCP}/\[ OK \] ${arquivoCP}/" $checkArquivosCP
+					
+					sed -i "s/>>> ${arquivoCP}/\[ OK \] ${arquivoCP}/" $checkarquivoControleContador
 
 					(( arquivoCont++ ))
+					(( l++ ))
+					# Auto increment da variável para a definição da linha a ser lida do arquivo de controle
 				fi
 		
 			else
 
 				(( dNumero++ ))
-				main
+				
+				local saidaWhile=0
+				arquivoCP=""
 
 			fi
 
-			(( l++ ))
-			# Auto increment da variável para a definição da linha a ser lida do arquivo de controle
 	done
+
+	if [[ $saidaWhile -eq 0 ]]; then
+		main
+	fi
 }
 
 main
